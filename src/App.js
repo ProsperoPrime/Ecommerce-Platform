2f7248b318d9a6d26a659c9c61be6b1a8a6e0cb6
Pranjal Kumar Maurya <123074864+PranjalKumarMaurya@users.noreply.github.com>
2023-06-21 16:30:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={} />
+         <Route path="/" element={} />
+         <Route path="/" element={} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
